aeb3853b9b227a74e6822fd7bfe1169967133086
Eric Dasse <christ.gououi@gmail.com>
2024-06-03 14:58:40 +0200

    Implement evaluation script

---
+++ iris_classification/evaluate.py
@@ -0,0 +1,37 @@
+import argparse
+import joblib
+import pandas as pd
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+
+def evaluate(iris_model, test_dataframe):
+    """Evaluate model on test data, and print the evaluation
+    metrics."""
+
+    X_test = test_dataframe.drop("species", axis=1).values
+    y_test = test_dataframe["species"].values
+
+    predictions = iris_model.predict(X_test)
+
+    accuracy = accuracy_score(y_true=y_test, y_pred=predictions)
+    precision = precision_score(
+        y_true=y_test,
+        y_pred=predictions,
+        average="binary",
+    )
+    recall = recall_score(y_true=y_test, y_pred=predictions, average="binary")
+
+    metrics = {"accuracy": accuracy, "precision": precision, "recall": recall}
+    print(metrics)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-path", "-m")
+    parser.add_argument("--test-dataset-path", "-d")
+
+    args = parser.parse_args()
+    iris_model = joblib.load(args.model_path)
+    test_dataframe = pd.read_csv(args.test_dataset_path)
+
+    evaluate(iris_model, test_dataframe)
